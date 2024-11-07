2e27ba3d85097bf23826a860e8461ffc5b309c24
Kim Sang Bin <sbkim@e-inspot.com>
2024-11-07 17:44:37 +0900

    commit-1107-2

---
+++ app/(guide_app)/guide/guide_doc/[comView]/contents/prototype-test-1.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+export default function ComViewContent() {
+  
+  return (
+    <div className='content-wrap'>
+
+      <div className='page-mobile-wrap'>
+        <div className='device-mobile-wrap'>
+          <iframe src="/prototype_mobile/" className="mobile-iframe">
+          </iframe>
+        </div>
+      </div>
+
+    </div>
+  );
+}
