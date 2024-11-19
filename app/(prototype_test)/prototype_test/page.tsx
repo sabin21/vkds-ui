b0d4c4a0228d96a188f9b4ca189124e65f53cb4c
Kim Sang Bin <sbkim@e-inspot.com>
2024-11-19 15:36:31 +0900

    commit-1119-03

--- app/(prototype_test)/prototype_test/page.tsx
+++ app/(prototype_test)/prototype_test/page.tsx
@@ -17,25 +17,25 @@ export default function TestMain() {
         <div className='block-title'>
           <h2>대구석 디자인 시안</h2>
           <p>
-            Bottom Bar의 메뉴 버튼을 클릭하면 메뉴화면이 열립니다.
+            하단네비게이션의 [메뉴] 버튼을 클릭하면 메뉴화면이 열립니다.
           </p>
         </div>
         <div className="inner-wrap">
           <div className="title-block">
-            <h4>Type-A</h4>
+            <h4>디자인 시안 A</h4>
             <div className='draft-desc'>
               <span className='strong'>
                 주요 서비스 및 콘텐츠 나열 형태의 구성
               </span>
               <ul>
                 <li>
-                메인화면 내 시의성 있는 여행 기사 소개 
-                </li>
-                <li>
-                주요 서비스 퀵 메뉴를 통한 빠른 접근 제공 
+                주요 서비스 퀵 메뉴를 통한 빠른 접근 제공
                 </li>
                 <li>
                 여행지, 축제, 이벤트 등 대한민국 구석구석의 주요 콘텐츠를 메인화면에서 모아보고 접근할 수 있음
+                </li>
+                <li>
+                메인화면 상단에 시의성 있는 여행기사 소개 (A안/B안 및 기존화면 모두 동일)
                 </li>
               </ul>
             </div>
@@ -44,21 +44,24 @@ export default function TestMain() {
             onClick={()=> handlePageChange('pageTest1')}
             className={`type-button ${activePage === 'pageTest1' ? 'active' : ''}`}
           >
-            Type-A 보기
+            A안 보기
           </button>
 
           <div className="title-block">
-            <h4>Type-B</h4>
+            <h4>디자인 시안 B</h4>
             <div className='draft-desc'>
               <span className='strong'>
                 지역 기반의 여행지/맛집/숙박 선택 구성
               </span>
               <ul>
                 <li>
-                메인화면 내 시의성 있는 여행 기사 소개
+                  지역별 여행지, 맛집, 숙박 정보를 메인화면에서 볼 수 있도록 구성
                 </li>
                 <li>
-                지역별 여행지, 맛집, 숙박 정보를 메인화면에서 볼 수 있도록 구성 
+                여행지, 축제 등 대한민국 구석구석의 주요 콘텐츠를 메인화면에서 모아보고 접근할 수 있음
+                </li>
+                <li>
+                메인화면 상단에 시의성 있는 여행기사 소개 (A안/B안 및 기존화면 모두 동일)
                 </li>
               </ul>
             </div>
@@ -67,7 +70,7 @@ export default function TestMain() {
             onClick={()=> handlePageChange('pageTest2')}
             className={`type-button ${activePage === 'pageTest2' ? 'active' : ''}`}
           >
-            Type-B 보기
+            B안 보기
           </button>
         </div>
       </div>
