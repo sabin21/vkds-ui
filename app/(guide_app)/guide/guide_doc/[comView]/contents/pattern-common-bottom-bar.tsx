7f49b775287f8603b1e50c2f75a47cab4099eb67
Kim Sang Bin <sbkim@e-inspot.com>
2024-12-18 04:33:20 +0900

    commit-1218-2

---
+++ app/(guide_app)/guide/guide_doc/[comView]/contents/pattern-common-bottom-bar.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+export default function ComViewContent() {
+    
+  return (
+    <div className='content-wrap'>
+ 
+    <div className="section-block level2">
+
+      {/* <h4>전체 검색, 여행지 목록</h4> */}
+      
+      <img src="/img/guide_doc/pattern/bottom_bar.png" className="full-img" />
+    </div>
+
+    </div>
+  );
+}
